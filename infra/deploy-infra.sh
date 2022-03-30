489cfed2dea3f9638d41ee9de0454b9c3b7963b5
JStenmark <johannes@stenmark.in>
2022-03-30 10:48:31 +0200

    load balancer

--- infra/deploy-infra.sh
+++ infra/deploy-infra.sh
@@ -42,10 +42,10 @@ aws cloudformation deploy \
     GitHubBranch=$GH_BRANCH \
     GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
-    
+
 # If the deploy succeeded, show the DNS name of the created instance
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap \
-    --query "Exports[?starts_with(Name,'InstanceEndpoint')].Value"
+    --query "Exports[?ends_with(Name,'LBEndpoint')].Value" 
 fi
